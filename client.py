755265bdfe3df943c7e37a442b3434d64d354e61
Alchemication <napora.adam@gmail.com>
2019-05-10 20:39:44 +0100

    Resizing images to 400x400 by default

--- client.py
+++ client.py
@@ -14,18 +14,22 @@ args = vars(ap.parse_args())
  
 # initialize the ImageSender object with the socket address of the
 # server
+print('[INFO] Connecting to imagezmq server on tcp://{}:5555'.format(args["server_ip"]))
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
    args["server_ip"]))
 
+print('[INFO] Warming up camera sensor...')
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
-rpiName = socket.gethostname()
+rpi_name = socket.gethostname()
 vs = VideoStream(usePiCamera=True).start()
 #vs = VideoStream(src=0).start()
-time.sleep(2.0)
- 
+time.sleep(1.0)
+
+res_dim = (400, 400)
+print('[INFO] Streaming video at res width {}...'.format(res_dim))
 while True:
     # read the frame from the camera and send it to the server
     frame = vs.read()
-    frame = resize(frame, width=400)
-    sender.send_image(rpiName, frame)
+    frame = resize(frame, width=res_dim[0], height=res_dim[0])
+    sender.send_image(rpi_name, frame)
